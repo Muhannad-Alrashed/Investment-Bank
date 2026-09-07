6ba1edf0dd2467f61440ad0164d0a925eedec5b1
Muhannad Al-Rashed <muhanned19alrashed93@gmail.com>
2026-09-07 13:05:08 +0300

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path/win32'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Investment-Bank',  // ← This must match your repo name
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
